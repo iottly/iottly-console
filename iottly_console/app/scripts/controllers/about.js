f7d15b9b4b895a7f97d570e1f35a79e555102001
raffaele <raffaele.passannanti@rafpas.it>
2015-11-27 16:42:20 +0100

    angular bootstrap mvc

---
+++ iottly_console/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name consoleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the consoleApp
+ */
+angular.module('consoleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ iottly_console/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name consoleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the consoleApp
+ */
+angular.module('consoleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
