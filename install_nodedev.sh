37eead5a00a2b9c5d69d2075fc7d8f8dbf7fad53
Stefano <stefano.terna@tomorrowdata.io>
2016-10-09 23:38:01 +0200

    added single command to install bower stuff after fresh clone

---
+++ install_nodedev.sh
@@ -0,0 +1,3 @@
+docker build -t iottlynodedev .
+cd iottly_console
+docker run -it -v `pwd`:/iottly_console iottlynodedev /bin/bash install.sh
---
+++ iottly_console/install.sh
@@ -0,0 +1,5 @@
+cd /iottly_console/iottly_main
+bower install
+
+cd /iottly_console/iottly_project
+bower install
